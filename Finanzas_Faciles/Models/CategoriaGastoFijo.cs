129d229cfd19be9b31cbc178e454879719f2ea7e
Alanjaqs <alannjaqs@gmail.com>
2026-02-24 20:35:14 -0300

    Models GastoFijo y CategoriaGastoFijo

---
+++ Finanzas_Faciles/Models/CategoriaGastoFijo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finanzas_Faciles.Models
+{
+    public enum CategoriaGastoFijo
+    {
+        Servicios,
+        Local,
+        Otros
+    }
+}
---
+++ Finanzas_Faciles/Models/GastoFijo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finanzas_Faciles.Models
+{
+    public class GastoFijo
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public decimal MontoMensual { get; set; }
+
+        public CategoriaGastoFijo Categoria { get; set; }
+
+        public bool Activo { get; set; } = true;
+    }
+}
