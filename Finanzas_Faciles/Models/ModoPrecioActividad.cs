b0abeed35570c9d61f5e9d848f8facc46bccee97
GitHubestudio <176661583+GitHubestudio@users.noreply.github.com>
2026-02-24 00:12:58 -0300

    agrego 3 models

---
+++ Finanzas_Faciles/Models/ModoPrecioActividad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finanzas_Faciles.Models
+{
+    public enum ModoPrecioActividad
+    {
+    PorMargen,
+    PrecioFijo
+    }
+}
