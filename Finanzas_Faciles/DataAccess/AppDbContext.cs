258ecd7a6660711c1f9a99106999559f7fd7926c
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2026-02-25 11:59:26 -0300

    Paquetes Microsoft.EntityFrameworkCore y Microsoft.EntityFrameworkCore.Sqlite y cree AppDbContext.cs

---
+++ Finanzas_Faciles/DataAccess/AppDbContext.cs
@@ -0,0 +1,65 @@
+using Microsoft.EntityFrameworkCore;
+using Finanzas_Faciles.Models;
+
+namespace Finanzas_Faciles.Services;
+
+public class AppDbContext : DbContext
+{
+    private static string DbPath => Path.Combine(FileSystem.AppDataDirectory, "proyectoreferencia.db3");
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public AppDbContext() : this(new DbContextOptionsBuilder<AppDbContext>()
+        .UseSqlite($"Filename={DbPath}")
+        .Options) { }
+
+    public DbSet<Item> Items => Set<Item>();
+    public DbSet<GastoFijo> GastosFijos => Set<GastoFijo>();
+    public DbSet<Actividad> Actividades => Set<Actividad>();
+    public DbSet<Ingreso> Ingresos => Set<Ingreso>();
+    public DbSet<Retiro> Retiros => Set<Retiro>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseSqlite($"Filename={DbPath}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Item>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Titulo).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.Descripcion).HasMaxLength(1000);
+        });
+
+        modelBuilder.Entity<GastoFijo>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Nombre).IsRequired().HasMaxLength(200);
+        });
+
+        modelBuilder.Entity<Actividad>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Nombre).IsRequired().HasMaxLength(200);
+            entity.Ignore(e => e.PrecioVentaSugerido);
+            entity.Ignore(e => e.MargenCalculado);
+            entity.Ignore(e => e.UtilidadPorUnidad);
+        });
+
+        modelBuilder.Entity<Ingreso>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.NombreActividad).IsRequired().HasMaxLength(200);
+        });
+
+        modelBuilder.Entity<Retiro>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Concepto).IsRequired().HasMaxLength(500);
+            entity.Property(e => e.EstadoPuntoEquilibrioAlMomento).HasMaxLength(100);
+        });
+    }
+}
