cd0d0e424b1abf88f058500e0539b4487151399a
Alanjaqs <alannjaqs@gmail.com>
2026-02-26 12:29:23 -0300

    Agrego DatabaseService.cs

---
+++ Finanzas_Faciles/Services/DatabaseService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finanzas_Faciles.Services
+{
+    public class DatabaseService
+    {
+        private readonly IDbContextFactory<AppDbContext> _factory;
+
+        public DatabaseService(IDbContextFactory<AppDbContext> factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await using var db = await _factory.CreateDbContextAsync();
+            await db.Database.EnsureCreatedAsync();
+        }
+
+        public Task<AppDbContext> CreateContextAsync() => _factory.CreateDbContextAsync();
+    }
+}
