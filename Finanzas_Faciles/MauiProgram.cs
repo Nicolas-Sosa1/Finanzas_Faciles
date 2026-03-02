76f5d8d5b0f76c3223bc1e64f32b57cb4ec19bd2
GitHubestudio <176661583+GitHubestudio@users.noreply.github.com>
2026-03-02 01:00:09 -0300

    agrego conexion a base de datos

--- Finanzas_Faciles/MauiProgram.cs
+++ Finanzas_Faciles/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Finanzas_Faciles.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Finanzas_Faciles
 {
@@ -15,8 +17,14 @@ namespace Finanzas_Faciles
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "proyectoreferencia.db3");
+            builder.Services.AddDbContextFactory<AppDbContext>(options =>
+                options.UseSqlite($"Filename={dbPath}"));
+
+            builder.Services.AddSingleton<DatabaseService>();
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
