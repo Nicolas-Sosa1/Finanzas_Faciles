7090e1d8737fd48bf5e473ae89d2707d46b192f5
GitHubestudio <176661583+GitHubestudio@users.noreply.github.com>
2026-03-26 01:28:08 -0300

    agrego helper booleano convertidor de estado

---
+++ Finanzas_Faciles/Helpers/BoolToActivoConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finanzas_Faciles.Helpers
+{
+    public class BoolToActivoConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value is bool b ? (b ? "Activo" : "Inactivo") : string.Empty;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
