2f24a8104ee4401b5b2d8f9bbac369db4688b224
Alanjaqs <alannjaqs@gmail.com>
2026-04-15 12:27:12 -0300

    EstadoToColorConverter + InverterBoolConverter

---
+++ Finanzas_Faciles/Helpers/InvertedBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finanzas_Faciles.Helpers
+{
+    public class InvertedBoolConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value is bool b ? !b : value;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value is bool b ? !b : value;
+        }
+    }
+}
