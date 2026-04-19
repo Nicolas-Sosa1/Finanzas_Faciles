907d11c28992c5a4ad4fa59226cc55bb36f446be
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2026-04-19 00:20:55 -0300

    DashboardPage terminado y conectado

---
+++ Finanzas_Faciles/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ProyectoReferencia")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ProyectoReferencia.Pages")]
